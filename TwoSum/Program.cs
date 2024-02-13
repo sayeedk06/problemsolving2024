2e466c0bedb411fde62821d224fe3517edc011a9
sayeedk06 <sayeed.k06@gmail.com>
2024-02-13 15:37:55 +1000

    Initial solution to two sum problem

---
+++ TwoSum/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int[] num = {2,7,11,15}
+            int target = 9
+
+            loop through each element in num
+                loop through element in num and compare the first loop elements with the second
+                    if matches the target then save the index of both the loop
+
+            */
+
+            int[] num = { 2, 7, 11, 15 };
+            int[] savedindex = new int[2];
+            int target = 17;
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                for (int j = i+1; j  < num.Length; j++)
+                {
+                    if (num[i] + num[j] == target)
+                    {
+                        savedindex[0] = i;
+                        savedindex[1] = j;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0;i < savedindex.Length; i++)
+            {
+                Console.WriteLine(savedindex[i]);
+                
+            }
+        }
+    }
+}
